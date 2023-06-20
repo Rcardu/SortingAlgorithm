07861b994c2b95e9c1325e7143350eb34d2a2350
ricardo <gq1090523745@163.com>
2023-06-20 11:36:50 +0800

    排序算法完结

--- SortingAlgorithm/Using.cpp
+++ SortingAlgorithm/Using.cpp
@@ -1,6 +1,8 @@
 #include"../utils/common.hpp"
 #include"QuickSort.h"
 #include"MergeSort.h"
+#include"CountiongSort.h"
+#include"RadixSort.h"
 int main(){
 
     /*vector<int>nums,a,b,c;
@@ -22,10 +24,15 @@ int main(){
     cs.quickSort(c,0,c.size()-1);
     cout<<"快速排序后顺序为："<<endl;
     printVector(c);*/
-    vector<int>nums={7,3,2,6,0,1,5,4};
+    /*vector<int>nums={7,3,2,6,0,1,5,4};
     mergrSort(nums,0,nums.size()-1);
     cout<<"排序后为： "<<endl;
-    printVector(nums);
+    printVector(nums);*/
+
+    vector<int>nums={1,0,1,2,0,4,0,2,2,4};
+    radixSort(nums);
+    cout<<"排序后的数组为:"<<endl;
+    for(int num:nums)cout<<num<<" ";
 
 
 
