0c922e25815995dfe946778a7ca44136ce0ca148
ricardo <12191884+Ricardo_M_Du@user.noreply.gitee.com>
2024-04-16 16:21:37 +0800

    修改

--- include/SortClass.h
+++ include/SortClass.h
@@ -2,13 +2,12 @@
  * @Author: Ricardo
  * @Date: 2024-04-13 21:48:05
  * @Last Modified by: ICEY
- * @Last Modified time: 2024-04-13 22:52:20
+ * @Last Modified time: 2024-04-16 16:21:08
  */
 #pragma once
 #include <algorithm>
 #include <iostream>
 #include <vector>
-
 
 namespace ICEY {
 class SortAlorithm {
@@ -20,14 +19,16 @@ class SortAlorithm {
   void bucketSort(std::vector<float>& nums);
   /*计数排序*/
   void countingSortNative(std::vector<int>& nums);
-  // TODO: sort
+  // 计数排序
   void countingSort(std::vector<int>& nums);
+  // 插入排序
   void insertSort(std::vector<int>& nums);
+  // 归并排序
   void mergr(std::vector<int>& nums, int left, int mid, int right);
   void mergrSort(std::vector<int>& nums, int left, int right);
 
  public:
-  /*note:快速排序*/
+  /*快速排序*/
   class QuickSort {
    private:
     void Swap(std::vector<int>& nums, int left, int right);
@@ -55,6 +56,7 @@ class SortAlorithm {
   };
 
  public:
+  // 基数排序
   int digit(int num, int exp);
   void countingSortDigit(std::vector<int>& nums, int exp);
   void radixSort(std::vector<int>& nums);
